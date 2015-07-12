d0272ff83419a8ce01968ee25cabedfc329aea88
camelot <felipecameloalves@gmail.com>
2015-07-11 22:55:55 -0300

    NPerfeito Felipe Alves e Felipe Melo second try

--- miscelanea.class.php
+++ miscelanea.class.php
@@ -1,5 +1,39 @@
 <?php
     class Miscelanea{
-love esa
+<?php
+   function pokopikaDiv($nu){
+ $divisores = array();
+    $metInf = floor($nu/2);
+  for($indio=1;$i<=$metInf;$indio++){
+   if(!($nu%$i))
+    $divisores[] = $indio;
+  }
+ return $divisores;
+}
+
+              function somaPerfeita($nu,$div){
+ $soma = 0;
+ foreach($div as $divisores){
+  $soma += $divisores;
+ } 
+ if($soma==$nu){
+  return true;
+ }else
+ {
+  return false;
+ }
+}
+$nu = $_GET["n"];
+$numervrauPerf = array();
+$i = 2; 
+
+while(count($numervrauPerf)<$nu){
+        $div = retornaDiv($indio);
+ if(somaPerfeita($indio,$div))
+      $numervrauPerf[] = $indio;
+ $indio += 2;
+}
+
+?>
     }
 ?>
